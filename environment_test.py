9d302017ba161e903e26829508c42442e3898489
Blood Killer <u3537635@connect.hku.hk>
2020-04-15 18:23:04 +0800

    Random Player written, since something wrong with the environment when running. Will further investigate

--- environment_test.py
+++ environment_test.py
@@ -23,20 +23,41 @@ class Player(ABC):
     def reset(self, episode = 0, side = 1):
         pass
 
-class RandomPlayer():
+#Didn't include state. May take a look later
+class RandomPlayer(Player):
     def __init__(self, env, name = 'RandomPlayer', seed= None):
         super().__init__(env, name)
         self._seed = seed
 
+    def get_next_action(self) -> int:
+        available_moves = self.env.available_moves()
+
+        if not available_moves:
+            raise ValueError('Unable to determine a valid move')
+
+        #Choose one move from list of available moves
+        action = random.choice(list(available_moves))
+
+        return action
+
     def reset(self, episode: int = 0, side: int = 1) -> None:
         # For reproducibility of the random
         random.seed(self._seed)
-        self._state = random.getstate()
 
-for action in actions:
-    temps.append(env.step(action))
+# for action in actions:
+#     temps.append(env.step(action))
+#     env.render()
+#     env.change_player()
+
+done = False
+
+while not done:
+    action = RandomPlayer.get_next_action()
+    result = env.step(action)
+    done = result.is_done()
     env.render()
     env.change_player()
+
 
 for temp in temps:
     print(temp)
