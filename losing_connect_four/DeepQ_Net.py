ca9c8c66e6f3736efaa57b2241f1e8f088fa1428
Blood Killer <u3537635@connect.hku.hk>
2020-04-15 15:39:05 +0800

    losing_connect_four for implementing experiments

---
+++ losing_connect_four/DeepQ_Net.py
@@ -0,0 +1,42 @@
+import tensorflow as tf
+
+from tf_agents.agents.dqn import dqn_agent
+from tf_agents.drivers import dynamic_step_driver
+from tf_agents.environments import suite_gym
+from tf_agents.environments import tf_py_environment
+from tf_agents.eval import metric_utils
+from tf_agents.metrics import tf_metrics
+from tf_agents.networks import q_network
+from tf_agents.policies import random_tf_policy
+from tf_agents.replay_buffers import tf_uniform_replay_buffer
+from tf_agents.trajectories import trajectory
+from tf_agents.utils import common
+
+""" Deep Q Network by tensorflow"""
+
+fc_layer_params = (100,)
+
+q_net = q_network.QNetwork(
+    train_env.observation_spec(),
+    train_env.action_spec(),
+    fc_layer_params=fc_layer_params)
+
+#Set up optimizer
+
+optimizer = tf.compat.v1.train.AdamOptimizer(learning_rate=LR)
+
+#DQN Agent
+train_step_counter = tf.Variable(0)
+
+agent = dqn_agent.DqnAgent(
+    train_env.time_step_spec(),
+    train_env.action_spec(),
+    q_network=q_net,
+    optimizer=optimizer,
+    td_errors_loss_fn=common.element_wise_squared_loss,
+    train_step_counter=train_step_counter)
+
+agent.initialize()
+
+eval_policy = agent.policy
+collect_policy = agent.collect_policy
