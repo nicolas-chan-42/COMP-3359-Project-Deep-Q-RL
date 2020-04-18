bfd8b28dc9adac25ff1afccbd82cd85bf97628e8
Blood Killer <u3537635@connect.hku.hk>
2020-04-18 16:39:52 +0800

    Modified main training loop for better reward distribution, and number of losses computed

--- environment_test.py
+++ environment_test.py
@@ -2,7 +2,6 @@ from collections import deque
 
 import gym
 import numpy as np
-import tensorflow as tf
 
 from losing_connect_four.player import RandomPlayer, DeepQPlayer
 
@@ -16,7 +15,7 @@ PARAMS = {"ENV_NAME": "ConnectFour-v1",
           "EPS_DECAY_STEPS": 10000,
           "GAMMA": 0.95,
           "LAMBDA": 0.001,
-          "N_EPISODES": 10,
+          "N_EPISODES": 1000,
           "N_STEPS_PER_TARGET_UPDATE": 1000}
 
 """ Main Training Loop """
@@ -29,84 +28,84 @@ all_rewards = np.zeros(PARAMS["N_EPISODES"], dtype=np.int16)
 
 # Setup players.
 random_player = RandomPlayer(env)
-with tf.device('/CPU:0'):
-    dq_player = DeepQPlayer(env, PARAMS)
+# with tf.device('/CPU:0'):
+dq_player = DeepQPlayer(env, PARAMS)
 
-    players = {1: random_player, 2: dq_player}
-    player_id = 1
-    trainee_id = 2
+players = {1: random_player, 2: dq_player}
+player_id = 1
+trainee_id = 2
 
-    # Logging for the episode
+# Logging for the training loop
+n_lose = 0
 
+# TODO: Save model
+# Inside ONE episode:
+for episode in range(PARAMS["N_EPISODES"]):
+    # Reset reward
     episode_reward = 0
 
-    # TODO: Save model
-    # Inside ONE episode:
-    for episode in range(PARAMS["N_EPISODES"]):
-        # noinspection PyRedeclaration
-        state = env.reset()
+    # noinspection PyRedeclaration
+    state = env.reset()
 
-        # Initialize players
+    # Player in position
+    player = players[player_id]
 
+    # Do one step ahead of the while loop
+    # Log state and action histories
+
+    # Initialize action history and perform first step
+    action = player.get_next_action(state, n_step=total_step)
+    action_hist = deque([action], maxlen=2)
+    next_state, reward, done, _ = env.step(action)
+
+    # Initialize the state history and save the state and the next state into the queue
+    state_hist = deque([state], maxlen=4)
+    state_hist.append(next_state)
+    state = next_state
+
+    # Change player and enter while loop
+    player_id = env.change_player()
+
+    player = players[player_id]
+
+    while not done:
+
+        action_hist.append(player.get_next_action(state, n_step=total_step))
+
+        # Take the first action in the queue
+        next_state, reward, done, _ = env.step(action_hist[-1])
+
+        # Store the resulting state to history
+        state_hist.append(next_state)
+
+        # Change player here
+        player_id = env.change_player()
         player = players[player_id]
 
-        # Do one step ahead of the while loop
-        # Log state and action histories
-
-        # Initialize action history and perform first step
-        action = player.get_next_action(state, n_step=total_step)
-        action_hist = deque([action], maxlen=2)
-        next_state, reward, done, _ = env.step(action)
-
-        # Initialize the state history and save the state and the next state into the queue
-        state_hist = deque([state], maxlen=4)
-        state_hist.append(next_state)
+        # Update DQN weights.
+        player.learn(state_hist[-3], action_hist[-2], state_hist[-1], reward, done,
+                     n_step=total_step)
+        # Update training result at the end for the next step
+        total_step += 1
         state = next_state
 
-        # Change player and enter while loop
-        player_id = env.change_player()
+        # if not done, add specific rewards to the player
+        if not done:
+            episode_reward += reward
 
-        player = players[player_id]
+        # env.render()
+    # TODO: Need to double check if the rewards are distributed correctly
 
-        while not done:
+    if player_id != trainee_id:  # Final step act by opponent of trainee
+        reward *= -1  # Reward for losing to trainee
+        n_lose += max(0, int(reward))  # zero if draw
 
-            action_hist.append(player.get_next_action(state, n_step=total_step))
+    episode_reward += reward
 
-            # Take the first action in the queue
-            next_state, reward, done, _ = env.step(action_hist[-1])
+    dq_player.learn(state_hist[-2], action_hist, state_hist[-1], reward, done,
+                    n_step=total_step)
 
-            # Store the resulting state to history
-            state_hist.append(next_state)
+    all_rewards[episode] = episode_reward
 
-            # Change player here
-            player_id = env.change_player()
-            player = players[player_id]
-
-            # Update DQN weights.
-            player.learn(state_hist[-3], action_hist[-2], state_hist[-1], reward, done,
-                         n_step=total_step)
-            # Update training result at the end for the next step
-            total_step += 1
-            state = next_state
-
-            # if not done, add specific rewards to the player
-            if not done:
-                episode_reward += reward
-
-            # env.render()
-        # TODO: Need to double check if the rewards are distributed correctly
-        # Change player at the end of one episode for reward distribution
-        player_id = env.change_player()
-        player = players[player_id]
-
-        if player_id == 2:  # Final step act by dq player
-            reward *= -1  # Award for winning
-
-        episode_reward += reward
-
-        dq_player.learn(state_hist[-2], action_hist, state_hist[-1], reward, done,
-                        n_step=total_step)
-
-        all_rewards[episode] = episode_reward
-
-    print(all_rewards.mean(), all_rewards.sum())
+print(all_rewards.mean(), all_rewards.sum())
+print(f"Number of losses: {n_lose}")
