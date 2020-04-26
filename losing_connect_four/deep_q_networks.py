1746c84001931322ad71d9ad7bcda37376b075b5
HKU-h3537512 <ncsy@connect.hku.hk>
2020-04-27 01:22:52 +0800

    Refactor and separate out Deep-Q Network from Deep-Q Model
    * Merge updates from master branch
    * Deep-Q Networks' implementation is put together in a single deep_q_networks.py, including network architecture, loss function, and optimizer.
    * Migrate the duty of DQN creation in deep_q_model.py to deep_q_networks.py
    * Migrate loss function and optimizer creation in load_model to deep_q_networks.py
    * Update function calls' parameters list in player.py
    * Add type hints

--- losing_connect_four/deep_q_networks.py
+++ losing_connect_four/deep_q_networks.py
@@ -1,38 +1,52 @@
+from abc import ABC, abstractmethod
+
+from tensorflow.keras.optimizers import RMSprop
+from tensorflow.keras.losses import mean_squared_error
+from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.layers import Flatten, Dense
 from tensorflow.keras.models import Sequential
-from tensorflow_addons.optimizers import AdamW
 
 
-class DeepQNetwork:
+class DeepQNetwork(ABC):
     """
-    Create a deep-Q neural network.
+    Network base-class.
     :return: Tensorflow Deep-Q neural network model.
     """
-    def create_model(self, observation_space, action_space, params,
-                     *args, **kwargs):
+
+    @abstractmethod
+    def create_network(self, observation_space, action_space, params,
+                       *args, **kwargs):
+        return NotImplementedError
+
+    @abstractmethod
+    def _create_network(self, observation_space, action_space, params,
+                        *args, **kwargs):
         raise NotImplementedError
 
-    def _create_model(self, observation_space, action_space, params,
-                      *args, **kwargs):
-        raise NotImplementedError
-
+    @abstractmethod
     def create_optimizer(self, params):
+        raise NotImplementedError
+
+    @abstractmethod
+    def create_loss_function(self):
         raise NotImplementedError
 
 
 class SimpleDeepFCQNetwork(DeepQNetwork):
-    def create_model(self, observation_space, action_space, params,
-                     *args, **kwargs):
-        model = self._create_model(
+    def create_network(self, observation_space, action_space, params,
+                       *args, **kwargs):
+        model = self._create_network(
             observation_space, action_space, params, *args, **kwargs)
 
         # Used Adam optimizer to allow for weight decay
+        loss_function = self.create_loss_function()
         optimizer = self.create_optimizer(params)
-        model.compile(loss="mse", optimizer=optimizer)
+
+        model.compile(loss=loss_function, optimizer=optimizer)
         return model
 
-    def _create_model(self, observation_space, action_space, params,
-                      *args, **kwargs):
+    def _create_network(self, observation_space, action_space, params,
+                        *args, **kwargs):
         obs_space_card = observation_space[0] * observation_space[1]
 
         model = Sequential()
@@ -44,5 +58,13 @@ class SimpleDeepFCQNetwork(DeepQNetwork):
         model.add(Dense(action_space, activation="linear"))
         return model
 
+    def create_loss_function(self):
+        return mean_squared_error
+
     def create_optimizer(self, params):
-        return AdamW(lr=params["LR"], weight_decay=params["LAMBDA"])
+        return Adam(lr=params["LR"], beta_2=params["LAMBDA"])
+
+
+class SimplerFCDQN(SimpleDeepFCQNetwork):
+    def create_optimizer(self, params):
+        return RMSprop(lr=params["LR"])
