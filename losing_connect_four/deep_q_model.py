0236e9c44d61bcce167d6c898daac438c4383463
HKU-h3537512 <ncsy@connect.hku.hk>
2020-04-27 15:55:47 +0800

    Abstract the classes in deep_q_networks.py and add support for CNN
    * Abstract Optimizers and Loss Functions
    * Make use of mixin classes for better code re-usability using dependency injection
    * Refactor out Adam and RMSProp optimizers
    * Refactor out MSE loss function
    * Add a draft CNN network from branch new_sequential_dqn
    * Add support for channel dimension for ABC DeepQNetwork in deep_q_networks.py
    * Add channel dimension to state and next_state batched arrays in experience_replay()
    * State explicitly axis of flipping in experience_replay()

--- losing_connect_four/deep_q_model.py
+++ losing_connect_four/deep_q_model.py
@@ -102,11 +102,15 @@ class DeepQModel:
         reward_batch = np.stack(batches.reward)
         done_batch = np.stack(batches.done)
 
+        # Add channel dimension.
+        state_batch = np.expand_dims(state_batch, axis=3)
+        next_state_batch = np.expand_dims(next_state_batch, axis=3)
+
         # Prepare flipped states and actions.
         # Flip state and action along y-axis of game board.
-        state_batch_flip = np.flip(state_batch, axis=-1)
-        action_batch_flip = np.flip(action_batch, axis=-1)
-        next_state_batch_flip = np.flip(next_state_batch, axis=-1)
+        state_batch_flip = np.flip(state_batch, axis=2)
+        action_batch_flip = np.flip(action_batch, axis=0)
+        next_state_batch_flip = np.flip(next_state_batch, axis=2)
 
         # Concatenate non-flip with flip batches.
         state_batch_w_flip = np.concatenate((state_batch, state_batch_flip),
--- losing_connect_four/deep_q_networks.py
+++ losing_connect_four/deep_q_networks.py
@@ -1,70 +1,154 @@
 from abc import ABC, abstractmethod
 
-from tensorflow.keras.optimizers import RMSprop
+from tensorflow.keras.layers import Flatten, Dense, Conv2D, Activation
 from tensorflow.keras.losses import mean_squared_error
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.layers import Flatten, Dense
 from tensorflow.keras.models import Sequential
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.optimizers import RMSprop
 
 
+# Abstract Basic Classes.
 class DeepQNetwork(ABC):
     """
-    Network base-class.
-    :return: Tensorflow Deep-Q neural network model.
+    Network Abstract Base Class / Mixin Class.
+
+    * create_network(...) returns the compiled network.
+    * _create_network(...) returns the network architecture.
+    * create_optimizer(...) returns the optimizer to be used.
+    * create_create_loss_function(...) returns the loss function to be used.
     """
 
-    @abstractmethod
     def create_network(self, observation_space, action_space, params,
                        *args, **kwargs):
-        return NotImplementedError
+        # Add channel dimension.
+        observation_space = (*observation_space, 1)
 
-    @abstractmethod
-    def _create_network(self, observation_space, action_space, params,
-                        *args, **kwargs):
-        raise NotImplementedError
-
-    @abstractmethod
-    def create_optimizer(self, params):
-        raise NotImplementedError
-
-    @abstractmethod
-    def create_loss_function(self):
-        raise NotImplementedError
-
-
-class SimpleDeepFCQNetwork(DeepQNetwork):
-    def create_network(self, observation_space, action_space, params,
-                       *args, **kwargs):
-        model = self._create_network(
+        net = self._create_network(
             observation_space, action_space, params, *args, **kwargs)
 
         # Used Adam optimizer to allow for weight decay
         loss_function = self.create_loss_function()
         optimizer = self.create_optimizer(params)
 
-        model.compile(loss=loss_function, optimizer=optimizer)
-        return model
+        net.compile(loss=loss_function, optimizer=optimizer)
+        return net
 
+    @abstractmethod
     def _create_network(self, observation_space, action_space, params,
                         *args, **kwargs):
-        obs_space_card = observation_space[0] * observation_space[1]
+        raise NotImplementedError
 
-        model = Sequential()
-        model.add(Flatten(input_shape=observation_space))
-        model.add(Dense(obs_space_card * 2, activation="relu"))
-        model.add(Dense(obs_space_card * 2, activation="relu"))
-        model.add(Dense(obs_space_card * 2, activation="relu"))
-        model.add(Dense(obs_space_card * 2, activation="relu"))
-        model.add(Dense(action_space, activation="linear"))
-        return model
+    @abstractmethod
+    def create_optimizer(self, params):
+        raise NotImplementedError
 
+    @abstractmethod
     def create_loss_function(self):
-        return mean_squared_error
+        raise NotImplementedError
 
+
+class OptimizerMixin(ABC):
+    """"Optimizer ABC"""
+    @abstractmethod
+    def create_optimizer(self, params):
+        raise NotImplementedError
+
+
+class LossFunctionMixin(ABC):
+    """"Loss Function ABC"""
+    @abstractmethod
+    def create_loss_function(self):
+        raise NotImplementedError
+
+
+# Optimizers.
+class OptimizerMixinAdam(OptimizerMixin):
     def create_optimizer(self, params):
         return Adam(lr=params["LR"], beta_2=params["LAMBDA"])
 
 
-class SimplerFCDQN(SimpleDeepFCQNetwork):
+class OptimizerMixinRMSProp(OptimizerMixin):
     def create_optimizer(self, params):
         return RMSprop(lr=params["LR"])
+
+
+# Loss Functions.
+class LossFuncMSEMixin(LossFunctionMixin):
+    def create_loss_function(self):
+        return mean_squared_error
+
+
+# Networks.
+class SimpleDeepFCQNetwork(OptimizerMixinAdam, LossFuncMSEMixin, DeepQNetwork):
+    """
+    Architecture:
+
+    - Flatten(input: observation_space)
+    - 5 Dense(2 * obs_space)
+    - Dense(output: action_space)
+
+    Optimizer: Adam;
+    Loss Function: MSE.
+    """
+    def _create_network(self, observation_space, action_space, params,
+                        *args, **kwargs):
+        # Compute number of slots in Connect-Four.
+        obs_space_card = observation_space[0] * observation_space[1]
+
+        net = Sequential()
+        net.add(Flatten(input_shape=observation_space))
+        net.add(Dense(obs_space_card * 2, activation="relu"))
+        net.add(Dense(obs_space_card * 2, activation="relu"))
+        net.add(Dense(obs_space_card * 2, activation="relu"))
+        net.add(Dense(obs_space_card * 2, activation="relu"))
+        net.add(Dense(action_space, activation="linear"))
+        return net
+
+
+class SimplerFCDQN(OptimizerMixinRMSProp, SimpleDeepFCQNetwork):
+    """
+    Architecture:
+
+    - Flatten(input: observation_space)
+    - 5 Dense(2 * obs_space)
+    - Dense(output: action_space)
+
+    Optimizer: RMSProp;
+    Loss Function: MSE.
+    """
+    pass
+
+
+class CnnDqn(LossFuncMSEMixin, OptimizerMixinAdam, DeepQNetwork):
+    """
+    Architecture:
+
+    - Conv2D(16, kernel_size=4, strides=2, padding="valid",
+             input_shape=observation_space)
+    - Activation("relu")
+    - Conv2D(32, kernel_size=3, strides=2, padding="same")
+    - Activation("relu")
+    - Conv2D(32, kernel_size=3, strides=2, padding="same")
+    - Activation("relu")
+    - Dropout(0.25)
+    - Flatten()
+    - Dense(action_space, activation="softmax")
+
+    Optimizer: Adam;
+    Loss Function: MSE.
+    """
+    def _create_network(self, observation_space, action_space, params,
+                        *args, **kwargs):
+        model = Sequential()
+        model.add(Conv2D(16, kernel_size=4, strides=2, padding="valid",
+                         input_shape=observation_space))
+        model.add(Activation("relu"))
+        # model.add(Conv2D(32, kernel_size=3, strides=2, padding="same"))
+        # model.add(Activation("relu"))
+        # model.add(Conv2D(32, kernel_size=3, strides=2, padding="same"))
+        # model.add(Activation("relu"))
+        # model.add(Dropout(0.25))
+        model.add(Flatten())
+        model.add(Dense(action_space, activation="softmax"))
+
+        return model
